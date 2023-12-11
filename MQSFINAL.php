105ab37d90b5c78c2325d74669dce339a5bd2a6f
YnaSophiaDR <ynasophia.delrosario.cics@ust.edu.ph>
2023-12-11 10:13:29 +0800

    frontend for MQS

--- MQSFINAL.php
+++ MQSFINAL.php
@@ -3,48 +3,56 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/MQStry.css" />
+   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
+    </script>
     <title>Scheduling Simulation</title>
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-        table, th, td {
-            border: 1px solid black;
-        }
-    </style>
 </head>
-<body>
-    <h2>Scheduling Simulation</h2>
 
-    <form method="post" action="">
-        <label for="arrival_time">Arrival Time:</label>
-        <input type="text" name="arrival_time">
+<body class="mt-4" style="background-color: black;" >
+    <div class="container">
+            <h2>Scheduling Simulation</h2>
+        <div class="mb-3">
+            <form method="post" action="">
+                <label for="arrival_time">Arrival Time:</label>
+                <input type="text" name="arrival_time" class="form-control" required>
+        </div>
 
-        <label for="burst_time">Burst Time:</label>
-        <input type="text" name="burst_time">
+        <div class="mb-3">
+                <label for="burst_time">Burst Time:</label>
+                <input type="text" name="burst_time" class="form-control" required>
+        </div>
 
-        <label for="queue">Queue:</label>
-        <input type="text" name="queue">
-
-        <br><br>
-
-        <input type="submit" name="submit" value="Simulate">
-    </form>
-
-    <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
+        <div class="mb-3">
+                <label for="queue">Queue:</label>
+                <input type="text" name="queue" class="form-control" required>
+        </div>
+                <br>
+            <input type="submit" name="submit" class="btn btn-primary">
+        </form>
+</br></br>
+        
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
         $arrivalTimes = isset($_POST['arrival_time']) ? explode(' ', trim($_POST['arrival_time'])) : [];
         $burstTimes = isset($_POST['burst_time']) ? explode(' ', trim($_POST['burst_time'])) : [];
         $queues = isset($_POST['queue']) ? explode(' ', trim($_POST['queue'])) : [];
 
+
         // FCFS Scheduling Algorithm
         $n = count($arrivalTimes);
+
 
         $waitingTimes = array_fill(0, $n, 0);
         $turnaroundTimes = array_fill(0, $n, 0);
 
+
         $completionTime = 0;
+
 
         for ($i = 0; $i < $n; $i++) {
             // If the process has not arrived yet, wait
@@ -52,21 +60,27 @@
                 $completionTime = $arrivalTimes[$i];
             }
 
+
             // Calculate waiting time for the current process
             $waitingTimes[$i] = $completionTime - $arrivalTimes[$i];
 
+
             // Calculate turnaround time for the current process
             $turnaroundTimes[$i] = $waitingTimes[$i] + $burstTimes[$i];
+
 
             // Update completion time for the next process
             $completionTime += $burstTimes[$i];
         }
 
+
         // Calculate average waiting time
         $averageWaitingTime = $n > 0 ? array_sum($waitingTimes) / $n : 0;
 
+
         // Calculate average turnaround time
         $averageTurnaroundTime = $n > 0 ? array_sum($turnaroundTimes) / $n : 0;
+
 
         // Display Gantt chart and table
         echo '<h3>Gantt Chart</h3>';
@@ -75,6 +89,10 @@
             echo 'P' . ($i + 1) . ' | ';
         }
         echo '</div>';
+        
+        echo '<br>';
+
+
 
         echo '<h3>Table</h3>';
         echo '<table>';
@@ -90,9 +108,12 @@
         }
         echo '</table>';
 
+
         echo '<p>Average Waiting Time: ' . $averageWaitingTime . '</p>';
         echo '<p>Average Turnaround Time: ' . $averageTurnaroundTime . '</p>';
     }
     ?>
+ 
+    </div>
 </body>
 </html>
